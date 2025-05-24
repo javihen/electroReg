08543515ac47f1a6a503d3b3fa155e63830c44ee
javihen <62959381+javihen@users.noreply.github.com>
2025-05-24 18:02:32 -0400

    feat modificacion 43 eliminacion del menu de opciones

--- src/includeheader.js
+++ src/includeheader.js
@@ -1,14 +1,15 @@
 // include.js
 document.addEventListener("DOMContentLoaded", () => {
-  const includes = document.querySelectorAll('[data-include]');
-  includes.forEach(el => {
-    const file = el.getAttribute('data-include');
+  const includes = document.querySelectorAll("[data-include]");
+
+  includes.forEach((el) => {
+    const file = el.getAttribute("data-include");
     fetch(file)
-      .then(res => res.text())
-      .then(html => {
+      .then((res) => res.text())
+      .then((html) => {
         el.innerHTML = html;
       })
-      .catch(err => {
+      .catch((err) => {
         el.innerHTML = "<p>Error al cargar el archivo: " + file + "</p>";
         console.error(err);
       });
