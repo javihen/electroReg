6c3424e0fafedfbaf6a5e8cb3a1e41854c6e6760
javihen <javi_hen@hotmail.com>
2025-05-24 11:26:57 -0400

    feat adicion 41 inclucion de la cabecera del sistema

---
+++ src/includeheader.js
@@ -0,0 +1,16 @@
+// include.js
+document.addEventListener("DOMContentLoaded", () => {
+  const includes = document.querySelectorAll('[data-include]');
+  includes.forEach(el => {
+    const file = el.getAttribute('data-include');
+    fetch(file)
+      .then(res => res.text())
+      .then(html => {
+        el.innerHTML = html;
+      })
+      .catch(err => {
+        el.innerHTML = "<p>Error al cargar el archivo: " + file + "</p>";
+        console.error(err);
+      });
+  });
+});
